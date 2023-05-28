76d9ced15f8136c4e28a789010ff2ccb7cf20180
cjpark1541 <cjpark1541@knu.ac.kr>
2023-05-28 21:05:45 +0900

    23.05.28_21:00

--- Lv0/Lv0_수열과 구간 쿼리 4.py
+++ Lv0/Lv0_수열과 구간 쿼리 4.py
@@ -1,11 +1,9 @@
 def solution(arr,queries):
     for i in range(len(queries)):
-        i_min=min(queries[i]); i_max=max(queries[i])
-        for j in range(i_min,i_max):
-            
-        arr[i_min],arr[i_max]=arr[i_max],arr[i_min]
+        s=queries[i][0]; e=queries[i][1]; k=queries[i][2]
+        for j in range(s,e+1):
+            if j%k==0: arr[j]=arr[j]+1
     return arr
 
-
 arr=[0, 1, 2, 4, 3]; queries=[[0, 4, 1],[0, 3, 2],[0, 3, 3]]
-#s=0, e=4, k=1 / s=0 e=3 k=2 / s=0 e=3 k=3
+print(solution(arr,queries))
